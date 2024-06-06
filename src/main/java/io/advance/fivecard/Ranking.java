6dfbb4e6b9b257d14d9d81523a4cbf0732d3213b
Tshaba Phomolo Benedict <benedicttshaba@gmail.com>
2024-06-06 12:16:26 +0200

    Use default values for the Ranking enum

--- src/main/java/io/advance/fivecard/Ranking.java
+++ src/main/java/io/advance/fivecard/Ranking.java
@@ -1,23 +1,25 @@
 package io.advance.fivecard;
 
 public enum Ranking {
-  HIGH_CARD,
-  STRAIGHT,
-  ONE_PAIR,
-  TWO_PAIR,
-  THREE_OF_A_KIND,
-  FLUSH,
-  FULL_HOUSE,
-  FOUR_OF_A_KIND,
-  STRAIGHT_FLUSH,
-  ROYAL_FLUSH,
-  ;
+  HIGH_CARD("High Card"),
+  STRAIGHT("Straight"),
+  ONE_PAIR("One Pair"),
+  TWO_PAIR("Two Pair"),
+  THREE_OF_A_KIND("Three of a Kind"),
+  FLUSH("Flush"),
+  FULL_HOUSE("Full House"),
+  FOUR_OF_A_KIND("Four of a Kind"),
+  STRAIGHT_FLUSH("Straight Flush"),
+  ROYAL_FLUSH("Royal Flush");
+  
+  private final String value;
+
+  Ranking(String value) {
+      this.value = value;
+  }
 
   @Override
   public String toString() {
-    String[] translations = {
-      "High Card", "Straight", "One Pair", "Two Pair", "Three of a Kind", "Flush", "Full House", "Four of a Kind", "Straight Flush", "Royal Flush"
-    };
-    return  translations[this.ordinal()];
+      return value;
   }
 }
